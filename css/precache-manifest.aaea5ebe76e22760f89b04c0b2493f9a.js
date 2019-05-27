a2544f3680694b00b798159963909ad9f50e92f8
Irene Burlet <irene.blt@free.fr>
2019-05-27 19:11:13 +0200

    checkbox surface properties and grpahic colors

---
+++ css/precache-manifest.aaea5ebe76e22760f89b04c0b2493f9a.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "69f70517d28e01e32a37f2eed04859ce",
+    "url": "/img/header.69f70517.jpg"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "70a843c208314fb9749da879c36411d6",
+    "url": "/img/logoNavigator.70a843c2.png"
+  },
+  {
+    "revision": "c49d536e71e2313d0dc2",
+    "url": "/css/chunk-vendors.1ccbb757.css"
+  },
+  {
+    "revision": "5c78973350d55a54bd9bc9cea1615a72",
+    "url": "/img/logoLBA.5c789733.png"
+  },
+  {
+    "revision": "fd8dc825a233edb153a5b96b0e935f63",
+    "url": "/img/footer.fd8dc825.jpg"
+  },
+  {
+    "revision": "32ac9e91b7d03d302a44",
+    "url": "/js/app.cfddf48f.js"
+  },
+  {
+    "revision": "c49d536e71e2313d0dc2",
+    "url": "/js/chunk-vendors.0becb9bb.js"
+  },
+  {
+    "revision": "b082261bc01d7910baec3c40c727cac3",
+    "url": "/img/metal-bg.b082261b.jpg"
+  },
+  {
+    "revision": "17ce1e9301ca14c019b5da7681112d0c",
+    "url": "/img/sky.17ce1e93.png"
+  },
+  {
+    "revision": "2d1e309adb24c1fcefa70354baca7c09",
+    "url": "/img/resetBtn.2d1e309a.svg"
+  },
+  {
+    "revision": "bb565fdd0215bb48eb83c5e7c09f996c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "32ac9e91b7d03d302a44",
+    "url": "/css/app.aaddf478.css"
+  }
+];
